f9a1835323475a8e8d8a9833f9f383d2a8d27622
Fazliddin Sultanov <f.sultonov@mail.de>
2025-08-30 13:13:28 +0200

    first commit on main

---
+++ src/r_global.R
@@ -0,0 +1,76 @@
+# ==============================================================================
+# R Script for Quarto Shiny file (uzbinde.qmd).                                |
+# by Fazliddin Sultonov                                                        |
+# ==============================================================================
+
+
+
+# load variables ===============================================================================
+
+source("src/r_var.R")
+
+# load data =====================================================================================
+data_raw <- read.csv2(file = "data/usbinde.csv", header = TRUE, sep = ";")
+
+# prapare data
+# for Page data ===========================================================
+dt <-
+  data_raw |> 
+  replace(data_raw == "-", 0) |>
+  mutate_at(-c(1:3), as.numeric) 
+  # separate(col = Stichtag, into = c("D", "M", "Year")) |> 
+  # select(-c("D","M")) 
+
+
+# hier letzte row leer, cols haben na, deswegen letzte row löschen
+dt <- na.omit(dt)                            # löscht alle rows mit NA
+# data <- data[complete.cases(data), ]       # gibt df zurück ohne rows mit NA
+# data <- data[rowSums(is.na(data)) == 0, ]  # gibt df zurück mit rows ohne NA
+
+# for furter analyste/Server ====================================================
+
+
+# rename colnames with customised names
+data <- 
+  dt |> 
+  separate(col = Stichtag, into = c("D", "M", "Year")) |> 
+  select(-c("D","M")) |> 
+  rename(any_of(lookup))
+
+# Data Frame for Server ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# test <-
+#   data |> 
+#   mutate_at("Year", as.integer) |> 
+#   rowwise() |> 
+#   mutate(Cnt = sum(c_across(4:length(colnames(data)))))
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+# Funnel chart ==================================================================================
+df_stat<-
+  data |> 
+  # mutate_at("Year", as.character()) |>
+  # filter(Year==2022) |> 
+  pivot_longer(!c(Year, BL, Geschlecht), names_to = "Status", values_to = "Cnt") |> 
+  mutate(
+    Cnt = case_when(
+      Geschlecht == "männlich" ~ Cnt * -1,
+      TRUE ~ Cnt) 
+  ) #|> ungroup()
+
+
+
+## Data for Line Plot ===================================================================================================
+# f_date <- as.integer(first(unique(data$Year)))
+# l_date <- as.integer(last(unique(data$Year)))
+df_time <-
+  data |> 
+  mutate_at("Year", as.integer) |> 
+  rowwise() |> 
+  mutate(Cnt = sum(c_across(4:length(colnames(data))))) |> 
+  select(c(Year, BL, Geschlecht, Cnt)) |> 
+  group_by(Year, Geschlecht) |> 
+  summarise(Total = sum(Cnt))
+
+
+
---
+++ src/r_var.R
@@ -0,0 +1,76 @@
+# ==============================================================================
+# R Script for Quarto Shiny file (r_global.R.qmd).                                |
+# by Fazliddin Sultonov                                                        |
+# ==============================================================================
+
+
+# Load libraries ===============================================================
+# package for install (if not) and load packages
+if (!require(pacman)) {
+  install.packages("pacman")
+  require("pacman")
+}
+#
+pacman::p_load(
+  #odbc,         # for db connect
+  tidyverse,    # for data manupulation, dplyr, ggplot, ...
+  lubridate,    # for data time
+  DT,           # for data table
+  glue,         # string
+  ggpol,        # für facet_share
+  ggpubr,       # ggdortchart, ggtexttable
+  gridExtra,    # Multiple plots and tables together
+  gt,           # Table
+  gtExtras,     # Support gt
+  svglite,      # for gt Tables
+  giscoR,       # for map
+  png,          # read png
+  ggimage,      # pictures to the plot
+  ggtext,       # markdown
+  ggpol         # facet
+)
+
+# Paths ==================================================================================
+# images --------------------
+img_dop <- readPNG("pics/dop.png")
+
+# Variables ==============================================================================
+lookup <- c(
+  BL = "Bundesland", 
+  AE_FreiG_EU = "Aufenthaltsrecht.nach.FreizügG.EU",
+  AE_Befreit = "Vom.Erfordernis.eines.Aufenthaltstitels.befreit", 
+  NE ="Unbefristete.Niederlassungserlaubnis",
+  AE_Befristet = "Befristete.Aufenthaltserlaubnis",
+  AE_Ausbildung = "Befristete.Aufenthaltserlaubnis..Ausbildung",
+  AE_Arbeit = "Befristete.Aufenthaltserlaubnis..Erwerbstätigkeit",
+  AE_Human = "Befristete.AE..völkerrechtl...human...pol..Gründe",
+  AE_Family = "Befristete.Aufenthaltserlaubnis..familiäre.Gründe",
+  National_Visa = "Befristete.AE..besondere.Gründe.und.nationale.Visa",
+  AE_Antrag = "Antrag.auf.einen.Aufenthaltstitel.gestellt",
+  AE_Gestattung = "Aufenthaltsgestattung",
+  AE_Ohne = "Ohne.Aufenthaltstitel..Duldung.oder.Gestattung"
+)
+
+# Abkürzung Bundesländer
+bl <- c("Schleswig-Holstein" = "SH", "Mecklenburg-Vorpommern"="MV", "Thüringen" = "TH", 
+"Niedersachsen" = "NI", "Baden-Württemberg" = "BW", "Nordrhein-Westfalen" = "NW", 
+"Rheinland-Pfalz" = "RP", "Saarland" = "SL", "Bayern" ="BY", "Berlin" ="BE", 
+"Sachsen" ="SN", "Brandenburg" ="BB", "Bremen" ="HB", "Hamburg" ="HH", 
+"Sachsen-Anhalt" = "ST", "Hessen" ="HE")
+
+# Plot ===================================================================================
+subt <- "*Data Source:* ©Statistisches Bundesamt (Destatis), 2025 | Stand: 31.07.2025"
+cap <- "*Plot by:* ***Fazliddin Sultonov***"
+
+# Theme standard
+my_theme <- theme(
+  plot.title = element_markdown(size = 14, color = "steelblue4", hjust = 0.5),
+  plot.subtitle = element_markdown(size = 12, hjust = 0.5),
+  plot.caption = element_markdown(size = 12, color = "steelblue4", hjust = 0.5),
+  legend.position = "inside", legend.position.inside = c(0.76, 0.07),
+  legend.direction = "horizontal",
+)
+
+# deploy
+#library(rsconnect)
+rsconnect::writeManifest()
